fa9314e967afecc5989fe64b005707d490eafe34
scarzze <hoseabiwott8@gmail.com>
2025-03-09 14:31:50 +0300

    last commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name , age ,sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 25, "M", "Brown" ,"Confident", "1"),
+(2, "Rowdy", 24, "M", "Black" ,"Creative", "1"), 
+(3, "Tabitha", 70, "F", "Brown" ,"Confident", "1"), 
+(4, "Segment Brown", 44, "M", "Brown" ,"Confident", "0"), 
+(5, "Melisa", 90, "F", "Black" ,"artistic", "0"), 
+(6, "Grinch",28, "M", "Brown" ,"Confident", "1"), 
+(7, "Wendy",47, "F", "Black" ,"mood", "0"), 
+(8, NULL, 87, "M", "Brown" ,"Confident", "1"); 
